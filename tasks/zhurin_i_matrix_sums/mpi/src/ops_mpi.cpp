91754598078a13bd7233c7b692afff38a335bfd1
VesTnik1320 <zhurin.ivan33@outlook.com>
2025-11-27 15:41:11 +0300

    Отчет

--- tasks/zhurin_i_matrix_sums/mpi/src/ops_mpi.cpp
+++ tasks/zhurin_i_matrix_sums/mpi/src/ops_mpi.cpp
@@ -28,23 +28,22 @@ bool ZhurinIMatrixSumsMPI::PreProcessingImpl() {
 bool ZhurinIMatrixSumsMPI::RunImpl() {
   int rank = 0;
   int wsize = 0;
-
   MPI_Comm_size(MPI_COMM_WORLD, &wsize);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
   int rows = static_cast<int>(std::get<0>(GetInput()));
   int columns = static_cast<int>(std::get<1>(GetInput()));
-
+  
   MPI_Bcast(&rows, 1, MPI_INT, 0, MPI_COMM_WORLD);
   MPI_Bcast(&columns, 1, MPI_INT, 0, MPI_COMM_WORLD);
   int size = rows * columns;
 
   int elems_per_proc = size / wsize;
   int remainder = size % wsize;
-
+  
   std::vector<int> counts(wsize, 0);
   std::vector<int> displs(wsize, 0);
-
+  
   int displ = 0;
   for (int proc = 0; proc < wsize; proc++) {
     counts[proc] = elems_per_proc + (proc < remainder ? 1 : 0);
@@ -52,15 +51,16 @@ bool ZhurinIMatrixSumsMPI::RunImpl() {
     displ += counts[proc];
   }
 
-  std::vector<double> matrix(size, 0);
-
+  std::vector<double> matrix;
   if (rank == 0) {
-    matrix = std::get<2>(GetInput());
+    matrix = std::get<2>(GetInput());  
   }
 
   std::vector<double> local_buff(counts[rank], 0);
 
-  MPI_Scatterv(matrix.data(), counts.data(), displs.data(), MPI_DOUBLE, local_buff.data(), counts[rank], MPI_DOUBLE, 0,
+  MPI_Scatterv(rank == 0 ? matrix.data() : nullptr,  
+               counts.data(), displs.data(), MPI_DOUBLE, 
+               local_buff.data(), counts[rank], MPI_DOUBLE, 0,
                MPI_COMM_WORLD);
 
   double local_sum = 0.0;
@@ -72,7 +72,6 @@ bool ZhurinIMatrixSumsMPI::RunImpl() {
   MPI_Allreduce(&local_sum, &global_sum, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
 
   GetOutput() = global_sum;
-
   return true;
 }
 
