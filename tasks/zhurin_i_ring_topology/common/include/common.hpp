0ec3267bdef0cf9ce6379fab3137cf6fb08dfad0
VesTnik1320 <zhurin.ivan33@outlook.com>
2025-12-22 17:14:49 +0300

    Fix memmory trouble v3

--- tasks/zhurin_i_ring_topology/common/include/common.hpp
+++ tasks/zhurin_i_ring_topology/common/include/common.hpp
@@ -1,21 +1,39 @@
 #ifndef ZHURIN_I_RING_TOPOLOGY_COMMON_INCLUDE_COMMON_HPP_
 #define ZHURIN_I_RING_TOPOLOGY_COMMON_INCLUDE_COMMON_HPP_
 
+#include <cstring>  // Для memset
 #include <tuple>
 #include <vector>
 
 namespace zhurin_i_ring_topology {
 
 struct RingMessage {
-  int source{};
-  int dest{};
-  std::vector<int> data;
-  bool go_clockwise = true;
+  int source{0};
+  int dest{0};
+  std::vector<int> data{};
+  bool go_clockwise{true};
 
-  RingMessage() = default;
+  // Конструктор, который обнуляет всю структуру
+  RingMessage() {
+    source = 0;
+    dest = 0;
+    go_clockwise = true;
+  }
 
-  RingMessage(int s, int d, std::vector<int> dta, bool gc)
-      : source(s), dest(d), data(std::move(dta)), go_clockwise(gc) {}
+  RingMessage(int src, int dst, std::vector<int> d, bool clockwise)
+      : source(src), dest(dst), data(std::move(d)), go_clockwise(clockwise) {}
+
+  // Явно обнуляем все байты при копировании
+  RingMessage(const RingMessage &other)
+      : source(other.source), dest(other.dest), data(other.data), go_clockwise(other.go_clockwise) {}
+
+  RingMessage &operator=(const RingMessage &other) {
+    source = other.source;
+    dest = other.dest;
+    data = other.data;
+    go_clockwise = other.go_clockwise;
+    return *this;
+  }
 };
 
 using InType = RingMessage;
@@ -24,4 +42,4 @@ using TestType = std::tuple<int, RingMessage>;
 
 }  // namespace zhurin_i_ring_topology
 
-#endif  // ZHURIN_I_RING_TOPOLOGY_COMMON_INCLUDE_COMMON_HPP_
+#endif
